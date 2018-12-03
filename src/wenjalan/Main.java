e2544abb42de7ccfffb7288ec44d9c49e54a358b
Alan-Wen <alanjameswen@outlook.com>
2018-12-02 21:44:25 -0800

    Updated NumberUtils
    Updated method getDigitAt() in NumberUtils to not use Strings, and added a new method lengthOf() to reutrn the number of digits in a number

--- src/wenjalan/Main.java
+++ src/wenjalan/Main.java
@@ -3,10 +3,7 @@ package wenjalan;
 public class Main {
 
     public static void main(String[] args) {
-        String alan100 = StringUtils.appendMult("alan", 5);
-        byte digit = NumberUtils.getDigitAt(10, 0);
-        System.out.println(alan100);
-        System.out.println(digit);
+        System.out.println(NumberUtils.getDigitAt(123_123_123_123_123L, 14));
     }
 
 }
--- src/wenjalan/NumberUtils.java
+++ src/wenjalan/NumberUtils.java
@@ -7,11 +7,17 @@ public class NumberUtils {
         throw new AssertionError("class is non-instantiable");
     }
 
-    // returns the digit at a given position of a number
-    public static byte getDigitAt(long num, int position) {
-        String numString = "" + num;
-        char digit = numString.charAt(position);
-        return Byte.parseByte("" + digit);
+    // returns the digit at a given place of a long
+    public static int getDigitAt(long num, int index) {
+        int length = lengthOf(num);
+        if (index < 0 || length < index) throw new IndexOutOfBoundsException("" + index);
+        int place = lengthOf(num) - index;
+        return (int) (num / ((int) Math.pow(10, place - 1)) % 10);
+    }
+
+    // returns the length of a long
+    public static int lengthOf(long num) {
+        return ("" + num).length();
     }
 
 }
